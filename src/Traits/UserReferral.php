dc06e720a03e9bb60e64f3cad0d29fcf7affe4f8
Noel Bradford <NoelBradford@users.noreply.github.com>
2019-05-19 15:57:23 +0100

    Added Functionality
    Added Definable Endpoints to getReferralLink() defaults to 'register?referral=' - see config/referral.php
    Added getRefererName() *Does what is says - Returns the users referers name*
    Added getReferredUsersCount() *Returns the number of times a users affilate code has been used*

--- src/Traits/UserReferral.php
+++ src/Traits/UserReferral.php
@@ -3,14 +3,14 @@
 /*
  * This file is part of questocat/laravel-referral package.
  *
- * (c) questocat <zhengchaopu@gmail.com>
+ * (c) emanci <zhengchaopu@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
 
 namespace Questocat\Referral\Traits;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Cookie;
 
@@ -18,8 +18,18 @@ trait UserReferral
 {
     public function getReferralLink()
     {
-        return url('/').'/?ref='.$this->affiliate_id;
+        return url(config('referral.referral_url','/register?referral=')).$this->affiliate_id;
     }
+        public function getRefererName()
+    {
+       $refer = DB::table('users')->where('affiliate_id',$this->referred_by)->first();
+        return $refer->name;
+    }
+     public function getReferredUsersCount() 
+     { 
+        $referred_users =  DB::table('users')->where('referred_by',$this->affiliate_id)->count(); 
+        return $referred_users;
+     }
 
     public static function scopeReferralExists(Builder $query, $referral)
     {
@@ -41,7 +51,7 @@ trait UserReferral
 
     protected static function generateReferral()
     {
-        $length = config('referral.referral_length', 5);
+        $length = config('referral.referral_length', 8);
 
         do {
             $referral = str_random($length);
